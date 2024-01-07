84e0aadfb2643a2ff41f7bca3bdb4eb1245296ab
adi1090x <adi1090x@gmail.com>
2024-01-07 14:50:12 +0530

    Updated Abif, Calamares, etc

--- calamares/install_dependencies.sh
+++ calamares/install_dependencies.sh
@@ -3,7 +3,7 @@
 # Install Calamares Deps
 
 sudo pacman -S --needed --noconfirm \
-		kconfig kcoreaddons kiconthemes ki18n kio solid yaml-cpp kpmcore kparts \
-		boost-libs hwinfo qt5-svg polkit-qt5 gtk-update-icon-cache plasma-framework \
+		kconfig5 kcoreaddons5 kiconthemes5 ki18n5 kio5 solid5 kparts5 yaml-cpp kpmcore \
+		boost-libs hwinfo qt5-svg polkit-qt5 gtk-update-icon-cache plasma-framework5 \
 		qt5-xmlpatterns squashfs-tools extra-cmake-modules qt5-tools qt5-translations git \
 		dmidecode boost mkinitcpio-openswap ckbcomp
